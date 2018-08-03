f284675bdba6d4b13bc56b819ed5870ffb854109
Carlos Octavio Preciado Lafarga <carloslafarga2244@gmail.com>
2018-08-03 15:07:29 -0700

    Formulario crear usuario
    creacion de formulario para registrar usiarios en angular 5

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '../../../node_modules/@angular/forms';
 import {Department } from '../models/department.model';
+import { BsDatepickerConfig } from '../../../node_modules/ngx-bootstrap/datepicker';
 
 
 @Component({
@@ -9,13 +10,28 @@ import {Department } from '../models/department.model';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
+  fotoview = false;
+  dateOfBirth:Date = new Date(2018,0,30);
+  datepickerConfig: Partial<BsDatepickerConfig>;
   departments: Department[] = [
     {id: 1, name:'IT'},
     {id: 2, name:'ADMIN'},
     {id: 3, name:'CHUY'},
     {id: 4, name:'CHOPIS'}
   ];
-  constructor() { }
+  constructor() { 
+
+    this.datepickerConfig = Object.assign({},{
+       containerClass:'theme-dark-blue',
+       showWeekNumbers:true,
+       minDate: new Date(2018,0,30),
+       maxDate :new Date(2018,11,31),
+       dateInputFormat: 'DD/MM/YYYY'
+    });
+  }
+  previewFoto(){
+    this.fotoview = !this.fotoview;
+  }
 
   ngOnInit() {
   }
